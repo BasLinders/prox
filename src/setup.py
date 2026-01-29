f5b729d549449f240d29063d08cdaf3235010e03
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2026-01-29 22:35:15 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -6,3 +6,6 @@ setup(
     ext_modules=cythonize("conformance.pyx"),
     include_dirs=[np.get_include()] # Essential if you use NumPy arrays in Cython
 )
+
+# When launching, run this ONCE in the terminal in the repository folder: python setup.py build_ext --inplace
+# The conformance file will be compiled to C.
